262f78fd138bf09a46b507a9bb7c343cb6eae185
huangyingjie <huangyingjie@meituan.com>
2017-01-06 11:04:39 +0800

    3D旋转立方体

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,66 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <canvas ref="canvas"></canvas>
   </div>
 </template>
 
 <script>
+/* global requestAnimationFrame */
+import THREE from 'three'
+import logo from '../assets/logo.png'
+
+let currentTime = Date.now()
 export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: ''
     }
+  },
+  methods: {
+    run () {
+      const duration = 5000
+      const animate = () => {
+        const now = Date.now()
+        const deltat = now - currentTime
+        currentTime = now
+        const fract = deltat / duration
+        const angle = Math.PI * 2 * fract
+        this.cube.rotation.y += angle
+      }
+      requestAnimationFrame(() => {
+        this.run()
+      })
+      this.renderer.render(this.scene, this.camera)
+      animate()
+    }
+  },
+  mounted () {
+    const { canvas } = this.$refs
+    const render = this.renderer = new THREE.WebGLRenderer({
+      canvas, antialias: true
+    })
+    render.setSize(canvas.width, canvas.height)
+    const scene = this.scene = new THREE.Scene()
+    const camera = this.camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 1, 4000)
+    scene.add(camera)
+
+    const loader = new THREE.TextureLoader()
+    const map = loader.load(logo)
+    const material = new THREE.MeshBasicMaterial({ map })
+    const geometry = new THREE.CubeGeometry(2, 2, 2)
+    const cube = this.cube = new THREE.Mesh(geometry, material)
+
+    cube.position.z = -8
+    cube.rotation.x = Math.PI / 5
+    cube.rotation.y = Math.PI / 5
+
+    scene.add(cube)
+    this.run()
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+canvas { width:300px; height:300px; }
 </style>
