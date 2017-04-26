8e8522ee94fa27a717cc892b196e7e63d7800f3d
huangyingjie <huangyingjie@meituan.com>
2017-04-26 17:41:15 +0800

    升级vue2

---
+++ src/components/Dir.vue
@@ -0,0 +1,33 @@
+<template lang='pug'>
+  div.hello
+    ul.clearfix.mx4(v-for="page in list")
+      li.left.mx2
+        router-link(:to="page.url") {{ page.title }}
+</template>
+
+<script>
+export default {
+  name: 'Dir',
+  data () {
+    return {
+      list: [{
+        url: '/basic',
+        title: 'WebGL basic'
+      }]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
